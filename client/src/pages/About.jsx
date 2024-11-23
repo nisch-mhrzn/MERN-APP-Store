bc48acc023a5c59edddf4cfd582caa22e72a5ad6
Nischal <nischal.maharjan1233@gmail.com>
2024-11-23 21:22:04 +0545

    Greetings added to about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,9 @@
 import { NavLink } from "react-router-dom";
 // import { Analytics } from "../components/Analytics";
-// import { useAuth } from "../store/auth";
+import { useAuth } from "../store/auth";
 
 export const About = () => {
-  // const { user } = useAuth();
+  const { user } = useAuth(); 
 
   return (
     <>
@@ -12,7 +12,7 @@ export const About = () => {
           <div className="container grid grid-two-cols">
             <div className="hero-content">
               {/* <p>We care to cure your Health</p> */}
-              
+              <p>Welcome ,{user?`${user.username} to our website` :`to our website`} </p>
               <h1>Why Choose Us? </h1>
               <p>
                 Expertise: Our team consists of experienced IT professionals who
